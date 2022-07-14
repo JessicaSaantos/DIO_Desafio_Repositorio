ec14efc239ac2ff7bac24abbc46920b571544651
JessicaSaantos <jessicaferre998@gmail.com>
2022-07-14 00:07:41 -0300

    adicionando melhorias na funcionalidade

--- script.js
+++ script.js
@@ -3,54 +3,54 @@ const titulo = document.querySelector('h3')
 const secao = document.querySelector('.notas')
 const formulario = document.querySelector('.formulario')
 
-// =========================================PASTAS=========================================
-const pasta1 = document.querySelector('#link1')
-const pasta2 = document.querySelector('#link2')
-const pasta3 = document.querySelector('#link3')
-const pasta4 = document.querySelector('#link4')
-
 // =========================================INICIANDO ANOTAÇÃO=========================================
-
-const tituloNota = document.querySelector('#titulo')
-const nota = document.querySelector('#anotacao')
 
 btn.addEventListener('click', addNovaNota)
 
 function addNovaNota(){
     
     formulario.classList.remove('esconder')
-
     const salvar = document.querySelector('#salvar')
     salvar.addEventListener('click' , salvarNota)
 
 }
+
+function cancelar() {
+    formulario.classList.add('esconder')
+}
+
 // =========================================SALVANDO INFORMAÇÕES=========================================
 
+const tituloNota = document.querySelector('#titulo')
+const nota = document.querySelector('#anotacao')
 let id = 0
 function salvarNota (){
+    
     formulario.classList.add('esconder')
     const titulo = tituloNota.value 
     const anotacao = nota.value 
 
-    if (titulo.typeof == null) {
+    if (anotacao == "") {
+       alert('Campo de anotação incompleto')
        
+    } else {
+        const novaNota = {
+            id: id,
+            nome: titulo,
+            nota: anotacao
+        }
+    
+        id++
+        listaAnotacoes.push(novaNota)
+    
+        redenrizarLayout()
+    
+        tituloNota.value = ""
+        nota.value = ""
+    
     }
 
-    const novaNota = {
-        id: id,
-        nome: titulo,
-        nota: anotacao
-    }
-
-    id++
-    listaAnotacoes.push(novaNota)
-
-    redenrizarLayout()
-
-    tituloNota.value = ""
-    nota.value = ""
-
-    console.log(listaAnotacoes)
+    
 }
 
 // =========================================CONSTRUINDO LAYOUT=========================================
@@ -106,24 +106,6 @@ function removerNota (event){
     listaAnotacoes.splice(posicaoRemovida, 1)
 
     redenrizarLayout()
-    console.log(listaAnotacoes)
 }
 
-// =========================================SELECIONADO PASTA=========================================
-const listaLinks = document.querySelector('.lista')
-listaLinks.addEventListener('click' , abrirPasta)
-const linksId = [pasta1 , pasta2 , pasta3, pasta4]
-function abrirPasta(event){
-    const link = event.target
 
-    if (link.tagName == "LI"){
-        const idLink = link.id
-
-        const linkEscolhido = linksId.find(
-            (link) => link.id == idLink
-           )
-           console.log(linkEscolhido)
-           titulo.innerText = linkEscolhido.innerText
-    }
-    
-}
